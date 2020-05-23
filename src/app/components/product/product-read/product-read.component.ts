cacd1e02881152dbbcc614bbb731bb61015bed24
George Matos <georgemattos@gmail.com>
2020-05-22 21:08:34 -0300

    criacao da coluna de edicao e exclusao de produtos

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -10,7 +10,7 @@ import { Product } from '../product.model';
 export class ProductReadComponent implements OnInit {
 
   produtos: Product[];
-  displayedColumns = ["id", "name", "price"];
+  displayedColumns = ["id", "name", "price", "actions"];
 
   constructor(
     private productService: ProductService
